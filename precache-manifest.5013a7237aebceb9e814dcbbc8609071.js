ffe329699f441f15716b089c43b3c251d0bb2557
noor <noormedia82@gmail.com>
2019-11-14 14:27:05 +0100

    Updates

---
+++ precache-manifest.5013a7237aebceb9e814dcbbc8609071.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3dfc2c2c61088a151626b1f9e1dfb29",
+    "url": "/reduxrandomusers/index.html"
+  },
+  {
+    "revision": "5c0db432da68497bcee2",
+    "url": "/reduxrandomusers/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "eca77084acb434e46357",
+    "url": "/reduxrandomusers/static/js/2.3354078c.chunk.js"
+  },
+  {
+    "revision": "5c0db432da68497bcee2",
+    "url": "/reduxrandomusers/static/js/main.3921715d.chunk.js"
+  },
+  {
+    "revision": "a66eef26ce78b6ba8666",
+    "url": "/reduxrandomusers/static/js/runtime-main.91de9b5d.js"
+  }
+]);
